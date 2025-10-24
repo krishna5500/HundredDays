9b5d8b1c5b8085a3b04b1353a21343b0abab56e9
Krishna <krishnakumar267383@gmail.com>
2025-10-24 20:48:14 +0530

    day2

---
+++ src/Day2/javaDataTypes.java
@@ -0,0 +1,29 @@
+package Day2;
+
+public class javaDataTypes
+{
+    public static void main(String[] args) {
+
+
+            String name = "Java";
+            char a = 'A';
+            boolean b = false;
+
+            byte b1 = 127;
+
+            float pi = 3.14165161565f;
+            double price = 99.994155165165;
+            System.out.println(pi);
+            System.out.println(price);
+
+            char unicodeDemo = '\u20AC';
+            System.out.println(unicodeDemo);
+
+            // Arrays
+            float[] marks = {1,2,3,4};
+            System.out.println(marks[0]);
+            System.out.println(marks[2]);
+
+        }
+    }
+
