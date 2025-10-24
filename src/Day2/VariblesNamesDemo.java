a3addd9b39aab52e3d37a041e47111c9cc8e8a2d
Krishna <krishnakumar267383@gmail.com>
2025-10-24 20:40:39 +0530

    day2

---
+++ src/Day2/VariblesNamesDemo.java
@@ -0,0 +1,21 @@
+package Day2;
+
+public class VariblesNamesDemo
+{
+    public static void main(String[] args) {
+        // valid varible names
+        int age=25;
+        int studentCount=100;
+        int _score=95;
+        int $total=500;
+        int gearRatio=6;
+        final int Max_SPEED=120;
+        //printing valid variables
+
+        System.out.println("age: "+age);
+        System.out.println("Student Count: "+studentCount);
+        System.out.println("Gear Ratio: "+gearRatio);
+        System.out.println("Max speed:" +Max_SPEED);
+
+    }
+}
